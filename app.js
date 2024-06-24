f85bf85e3fbf55682a8b557a5fdf64b147535f35
Eglknght <elang1089@gmail.com>
2024-06-24 20:33:45 +0700

    ritual

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console log('hello world')
+const express = require('express')
+const app = express()
+const port = 3000
+const c_beranda = require('./controller/c_beranda')
+
+
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'ejs')
+app.set('views', './view-html')
+
+app.get('/', c_beranda.index)
+
+
+app.listen(port, () => {
+    console.log(`aplikasi sudah siap, buka http://localhost:${port}`)
+})
