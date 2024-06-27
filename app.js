2647056802c4f7e58196db038d1ec2a3fa1b0d07
Eglknght <elang1089@gmail.com>
2024-06-27 21:43:30 +0700

    template view

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(session({
     resave: true,
     saveUninitialized: false,
     cookie: {
-        maxAge: 1000 * 60 * 60
+        maxAge: 1000 * 60 * 60 * 2
     }
 }))
 
--- controller/c_dashboard.js
+++ controller/c_dashboard.js
@@ -1,5 +1,5 @@
 module.exports = {
     index: (req, res) => {
-        res.render('dashboard/index')
+        res.render('template/struktur')
     }
 }
