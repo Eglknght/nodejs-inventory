aca30b0af805845320d477990be1470dded37d66
Eglknght <elang1089@gmail.com>
2024-06-27 20:17:23 +0700

    mvc dashboard

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require('express-session')
 
 const c_beranda = require('./controller/c_beranda')
 const c_auth = require('./controller/c_auth')
+const c_dashboard = require('./controller/c_dashboard')
 const cek_login = c_auth.cek_login
 
 
@@ -31,10 +32,7 @@ app.use(express.static('public'))
 app.get('/', c_beranda.index)
 app.get('/login', c_auth.form_login)
 app.post('/proses-login', c_auth.proses_login)
-
-app.get('/dashboard', cek_login, (req, res) => {
-    res.send('<h1>Halaman Dashboard</h1>')
-})
+app.get('/dashboard', cek_login, c_dashboard.index)
 
 
 
