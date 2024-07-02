f967b8b2c65dc7701a4e36cd27b9d439b29a0c69
Eglknght <elang1089@gmail.com>
2024-07-02 21:04:44 +0700

    tambahan config database

--- model/m_produk.js
+++ model/m_produk.js
@@ -1,11 +1,5 @@
 const mysql     = require('mysql2')
-const db        = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'xdb_belajar_database',
-})
-db.connect()
+const db        = require('../config/database').db
 
 
 module.exports =
