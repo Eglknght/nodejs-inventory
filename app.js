68c3d9a7df9a9bbfe8d605111f39851fc1426cb3
Eglknght <elang1089@gmail.com>
2024-07-05 03:25:30 +0700

    masukan route dalam 1 grup

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require('express-session')
 
 const c_beranda = require('./controller/c_beranda')
 const c_auth = require('./controller/c_auth')
-const c_dashboard = require('./controller/c_dashboard')
+const c_dashboard = require('./controller/c_dashboard') 
 const c_user = require('./controller/c_user')
 const c_master_produk = require('./controller/c_master_produk')
 const c_stok = require('./controller/c_stok')
@@ -19,7 +19,7 @@ app.use(session({
     secret: 'secret',
     resave: true,
     saveUninitialized: false,
-    cookie: {
+    cookie: {    
         maxAge: 1000 * 60 * 60 * 2
     }
 }))
@@ -31,20 +31,27 @@ app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
 app.set('views', './view-html')
 app.use(express.static('public'))
+const router = express.Router()
 
 app.get('/', c_beranda.index)
 app.get('/login', c_auth.form_login)
 app.post('/proses-login', c_auth.proses_login)
-app.get('/dashboard', cek_login, c_dashboard.index)
-app.get('/user-management', cek_login, c_user.index)
-app.get('/master-produk', cek_login, c_master_produk.index)
-app.get('/master-produk/tambah', cek_login, c_master_produk.form_tambah)
-app.post('/master-produk/proses-simpan', cek_login, c_master_produk.proses_simpan)
-app.get('/user/tambah', cek_login, c_user.form_tambah)
-app.post('/user/proses-simpan', cek_login, c_user.proses_simpan)
-app.get('/stok-masuk', cek_login, c_stok.form_stok_masuk)
-app.post('/stok-masuk/proses-simpan', cek_login, c_stok.proses_stok_masuk)
 
+// Apply middleware to the router
+router.use(cek_login)
+
+// Define routes within the router
+router.get('/dashboard', c_dashboard.index)
+router.get('/user-management', c_user.index)
+router.get('/master-produk', c_master_produk.index)
+router.get('/master-produk/tambah', c_master_produk.form_tambah)
+router.post('/master-produk/proses-simpan', c_master_produk.proses_simpan)
+router.get('/user/tambah', c_user.form_tambah)
+router.post('/user/proses-simpan', c_user.proses_simpan)
+router.get('/stok-masuk', c_stok.form_stok_masuk)
+router.post('/stok-masuk/proses-simpan', c_stok.proses_stok_masuk)
+
+app.use('/', router)
 
 app.listen(port, () => {
     console.log(`aplikasi sudah siap, buka http://localhost:${port}`)
