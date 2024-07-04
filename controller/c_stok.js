fb61c0e95e70cfb6c7d4ec93f4e9f14f4e937f22
Eglknght <elang1089@gmail.com>
2024-07-05 03:23:32 +0700

    perbaikan input stok masuk

--- controller/c_stok.js
+++ controller/c_stok.js
@@ -14,16 +14,29 @@ module.exports = {
 
     proses_stok_masuk: async (req, res) => {
         try {
-            let insert = await m_stok.input_stok_masuk(req)
+            let stok_terakhir = await m_stok.cek_stok_sisa(req.body.form_produk)
+            let sisa_terakhir = 0
+            
+            if (stok_terakhir.length > 0) {
+                sisa_terakhir = stok_terakhir[0].stok_sisa
+
+            }
+
+            let stok_akhir = sisa_terakhir + Number(req.body.form_jumlah)
+            console.log(sisa_terakhir)
+            let insert = await m_stok.input_stok_masuk(req, stok_akhir)
             let isi_notif = `berhasil menambahkan stok`
             if (insert.affectedRows > 0) {
                 res.redirect(`/stok-masuk?note=sukses&pesan=${isi_notif}`)
             }
+
         } catch (error) {
+            // console.log(error)
             let dataview = {
                 konten: 'stok/form-stok-masuk',
                 uri_segment: req.path.split('/'),
-                info_error: error
+                info_error: error,
+                produk: await m_produk.get_semua_produk(),
             }
             res.render('template/struktur', dataview)
         }
--- model/m_stok.js
+++ model/m_stok.js
@@ -1,18 +1,37 @@
 const mysql     = require('mysql2')
 const db        = require('../config/database').db
 const moment    = require('moment')
+
 moment.locale('id')
 
 module.exports =
 {
-    input_stok_masuk : (req) => {
+    cek_stok_sisa : (kode_produk) => {
+        let sql = mysql.format(
+            `SELECT * FROM stok 
+            WHERE kode_produk = ?
+            ORDER BY id DESC 
+            LIMIT 1`,
+            [kode_produk]
+        )
+        return new Promise((resolve, reject) => {
+            db.query(sql, function(errSql, hasil){
+                if(errSql){
+                    reject(errSql)
+                } else {
+                    resolve(hasil)
+                }
+            })
+        })
+    },
+
+    input_stok_masuk : (req, stok_akhir) => {
         let masuk = req.body.form_jumlah
-        let sisa = masuk
         let data = {
             kode_produk : req.body.form_produk,
             stok_masuk : req.body.form_jumlah,
             stok_keluar : 0,
-            stok_sisa : sisa,
+            stok_sisa : stok_akhir,
             keterangan : req.body.form_keterangan,
             dibuat_oleh : req.session.user.id,
             dibuat_kapan : moment().format('YYYY-MM-DD HH:mm:ss')
