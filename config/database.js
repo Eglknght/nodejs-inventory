57797c3ad43dbffa1c7115764fa78ad1400f1d23
Elang Satria Pratama <elang1089@gmail.com>
2026-09-08 02:45:56 +0700

    dotenv

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const mysql     = require('mysql2')
 const db        = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'xdb_belajar_database',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'xdb_belajar_database',
 })
 db.connect()
 
