1ae437779cfc84c8af88dcfe1643a0c6f68fda65
Elang Satria Pratama <elang1089@gmail.com>
2026-09-12 11:34:29 +0700

    ganti createConnection dengan createPool

--- config/database.js
+++ config/database.js
@@ -1,9 +1,12 @@
 const mysql     = require('mysql2')
-const db        = mysql.createConnection({
+const db        = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'xdb_belajar_database',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
 db.connect()
 
